4e8dabea7a147fc0f1904058657ffeeb0adaaa87
Julia Sexton <juliasexton.cs@gmail.com>
2026-08-30 17:42:27 -0400

    Added a new method called heyNow()

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -2,6 +2,10 @@ package ver1;
 
 public class HelloWorld {
 
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Abranch was added and I am working locally on the branch.");
